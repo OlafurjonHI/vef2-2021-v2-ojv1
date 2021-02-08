717ec386a46aefc2336a8e15b44dc2d0b65b69c5
OlafurjonHI <ojv1@hi.is>
2021-02-08 09:29:38 +0000

    Einhver db virkni komin ásamt villusíðu og 404 síðu, mun þetta virka á heroku?

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import validator from 'express-validator';
+import { RetrieveSignatures, InsertSignatures } from './db.js';
 
 const { body, validationResult } = validator;
 
@@ -10,15 +11,38 @@ const {
   PORT: port = 3001,
 } = process.env;
 
+const toNormalDate = (date) => `${date.getDate()}.${(date.getMonth() + 1)}.${date.getFullYear()}`;
+
 const dirname = path.resolve();
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.render('index', { errorMessages: null });
+app.use('/', (req, res, next) => {
+  if (res.statusCode === 500) {
+    res.render('error');
+  } else next();
+});
+app.locals.errorLocation = [];
+app.locals.list = [];
+app.locals.normalDate = toNormalDate;
+app.get('/', async (req, res) => {
+  app.locals.errorLocation = [];
+  const {
+    name = '',
+    ssn = '',
+    comment = '',
+    anon,
+  } = req.body;
+  app.locals.name = name;
+  app.locals.ssn = ssn;
+  app.locals.comment = comment;
+  app.locals.anon = anon;
+  await RetrieveSignatures().then((d) => {
+    app.locals.list = d.rows;
+  });
+  res.render('index', { errorMessages: null, data: app.locals });
 });
 app.post(
   '/',
@@ -34,24 +58,58 @@ app.post(
   body('ssn')
     .isLength({ min: 10, max: 11 })
     .withMessage('Kennitala verður að vera á formi 000000-0000 eða 0000000000'),
-  (req, res, next) => {
+  body('comment')
+    .isLength({ max: 400 })
+    .withMessage('Athugasemd má að hámarki vera 400 stafir'),
+  // eslint-disable-next-line consistent-return
+  async (req, res, next) => {
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
+      const {
+        name,
+        ssn,
+        comment,
+        anon,
+      } = req.body;
+      app.locals.name = name;
+      app.locals.ssn = ssn;
+      app.locals.comment = comment;
+      app.locals.anon = anon;
+      app.locals.errorLocation = errors.array().map((i) => i.param);
+      await RetrieveSignatures().then((d) => {
+        app.locals.list = d.rows;
+      });
       const errorMessages = errors.array().map((i) => i.msg);
-      return res.render('index', { errorMessages });
+      return res.render('index', { errorMessages, data: app.locals });
     }
+    app.locals.errorLocation = [];
     next();
   },
   body('name').trim().escape(),
   body('ssn').blacklist('-'),
-  (req, res) => {
+  body('comment').trim().escape(),
+  async (req, res, next) => {
     const {
       name,
       ssn,
       comment,
+      anon,
     } = req.body;
-    res.render('index', { errorMessages: null });
+    app.locals.name = '';
+    app.locals.ssn = '';
+    app.locals.comment = '';
+    app.locals.anon = undefined;
+    const letAnon = anon !== undefined;
+    await InsertSignatures([name, ssn, comment, letAnon]).then(async () => {
+      await RetrieveSignatures.then((d) => {
+        app.locals.list = d.rows;
+      });
+    }).then(() => {
+      res.redirect(308, '/');
+    }).catch(() => {
+      res.status(500);
+      res.render('error');
+    });
   },
 );
 
--- src/db.js
+++ src/db.js
@@ -13,3 +13,42 @@ if (!connectionString) {
 }
 
 // TODO gagnagrunnstengingar
+const pool = new pg.Pool({ connectionString });
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+const RetrieveSignatures = async () => {
+  const client = await pool.connect();
+  let rows = [];
+  try {
+    rows = await client.query('SELECT * FROM Signatures');
+  } catch (e) {
+    console.error('Error selecting', e);
+  } finally {
+    client.release();
+  }
+  // await pool.end();
+  return rows;
+};
+
+const InsertSignatures = async (values) => {
+  const client = await pool.connect();
+  const query = 'INSERT INTO Signatures(name,nationalId, comment,anonymous) VALUES($1, $2, $3,$4) RETURNING *';
+  let result = [];
+  try {
+    result = await client.query(query, values);
+  } catch (e) {
+    console.error('Error Inserting Data', e);
+  } finally {
+    client.release();
+  }
+  // await pool.end();
+  return result;
+}
+
+
+export { RetrieveSignatures, InsertSignatures};
+
+export default RetrieveSignatures;
